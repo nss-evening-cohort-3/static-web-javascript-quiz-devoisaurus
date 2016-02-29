c7d99864f6c40552eb0b2d4b66ae41c4e3993bfe
Andi Devois <devoisaurus@Andi-Devoiss-MacBook-Pro.local>
2016-02-29 16:37:12 -0600

    deleted obsolote js file, removed redundancies, cleaned up quiz2 js

--- quiz2.js
+++ quiz2.js
@@ -1,28 +1,28 @@
 // Declaring variable for click event
 var submitButton = document.getElementById("button");
-// Setting up empty object to pass key values to
-var treeObject = {};
-// Declaring variable for spacing
-var line = "";
-// used for validation function
-var heightInput = document.getElementById("height");
-var charInput = document.getElementById("character");
+
 
 // validates that both fields are complete.  if not, alert pops up, if both complete, the function runs
 function validate() {
+ // validating user input
+  var heightInput = document.getElementById("height");
+  var charInput = document.getElementById("character");
+ // empty object for key values 
+  var treeObject = {};
+ // spacing variable
+  var line = "";
   if (((heightInput.value).length === 0) || ((charInput.value).length === 0)) {
     alert("Please enter both a height and a character!")
   }
   else {
-  	treeObject.height = document.getElementById("height").value;
-		treeObject.character = document.getElementById("character").value;
-		// console.log("test", treeObject);
-  	buildTree(treeObject);
+    treeObject.height = document.getElementById("height").value;
+    treeObject.character = document.getElementById("character").value;
+    buildTree(treeObject);
   }
  }
 
-// function to build tree in console with mathy things
-function buildTree() {
+// function to build tree in console with values passed in from validation
+function buildTree(treeObject) {
  for (var i = 1; i <= treeObject.height; i ++) {
      line=""; 
     for (var j = 0; j <= treeObject.height-i-1; j++) {
